79375bfde6b15b3c3425c837fb2f09ab275dc351
neilpreyes <nlreyes@cpp.edu>
2023-05-02 18:44:47 -0700

    Format time and date

--- Scheduler/Schedule.java
+++ Scheduler/Schedule.java
@@ -1,5 +1,10 @@
 package Scheduler;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Schedule {
 
     /**
@@ -54,9 +59,26 @@ public class Schedule {
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws ParseException{
         boolean test = editTask("Hello");
         System.out.println(test);
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        SimpleDateFormat stf = new SimpleDateFormat("HH:mm");
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        c.add(Calendar.DATE, 5);
+        String time = "23:55";
+        Date d = stf.parse(time);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(d);
+        cal.add(Calendar.MINUTE, 10);
+        String newTime = stf.format(cal.getTime());
+        String output = sdf.format(c.getTime());
+        System.out.println(output);
+        System.out.println(newTime);
+
     }
 
 }
