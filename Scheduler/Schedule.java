ec26ea0bc325d9dfad52d8367e119b1ad347c447
Kevin Kim <kevink000908@gmail.com>
2023-05-07 22:15:34 -0700

    test

--- Scheduler/Schedule.java
+++ Scheduler/Schedule.java
@@ -2,10 +2,14 @@ package Scheduler;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Schedule
 {
+    private ArrayList taskList;
+
+
 
 }
