ed2d8b312f15fc6c1e26b51ee1d429ab32deb285
Ericcyyyy <eric13lld@gmail.com>
2023-05-07 22:41:49 -0700

    updated antitask

--- Scheduler/Task.java
+++ Scheduler/Task.java
@@ -4,5 +4,14 @@ public class Task
 {
     String taskName;
 
+    // Start Time
     double duration;
+}
+
+class AntiTask extends Task
+{
+    public static void cancelRecurringTask(String taskName, String date, double startTime, double duration)
+    {
+
+    }
 }
